16c8c1cc810b99f5386c95d67da34a367a99ef2b
Rebecca Chen <rebecca061996@gmail.com>
2021-03-02 19:56:38 +0000

    add queries to get line items and products in line order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,13 @@
 class OrdersController < ApplicationController
-
   def show
     @order = Order.find(params[:id])
+    @line_items = LineItem.where('order_id = ?', @order.id)
+    @products = @line_items.map { |item| Product.find_by id: item.product_id }
   end
 
   def create
     charge = perform_stripe_charge
-    order  = create_order(charge)
+    order = create_order(charge)
 
     if order.valid?
       empty_cart!
@@ -14,7 +15,6 @@ class OrdersController < ApplicationController
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
-
   rescue Stripe::CardError => e
     redirect_to cart_path, flash: { error: e.message }
   end
@@ -28,19 +28,20 @@ class OrdersController < ApplicationController
 
   def perform_stripe_charge
     Stripe::Charge.create(
-      source:      params[:stripeToken],
-      amount:      cart_subtotal_cents,
+      source: params[:stripeToken],
+      amount: cart_subtotal_cents,
       description: "Khurram Virani's Jungle Order",
-      currency:    'cad'
+      currency: 'cad',
     )
   end
 
   def create_order(stripe_charge)
-    order = Order.new(
-      email: params[:stripeEmail],
-      total_cents: cart_subtotal_cents,
-      stripe_charge_id: stripe_charge.id, # returned by stripe
-    )
+    order =
+      Order.new(
+        email: params[:stripeEmail],
+        total_cents: cart_subtotal_cents,
+        stripe_charge_id: stripe_charge.id, # returned by stripe
+      )
 
     enhanced_cart.each do |entry|
       product = entry[:product]
@@ -49,11 +50,10 @@ class OrdersController < ApplicationController
         product: product,
         quantity: quantity,
         item_price: product.price,
-        total_price: product.price * quantity
+        total_price: product.price * quantity,
       )
     end
     order.save!
     order
   end
-
 end
