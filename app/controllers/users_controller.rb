e685df99ef8ab45bce88ebfa71435c60be2cb5ed
Rebecca Chen <rebecca061996@gmail.com>
2021-03-03 19:05:20 +0000

    add action for creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,27 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create; end
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+
+  def user_params
+    params
+      .require(:user)
+      .permit(
+        :first_name,
+        :last_name,
+        :email,
+        :password,
+        :password_confirmation,
+      )
+  end
 end
