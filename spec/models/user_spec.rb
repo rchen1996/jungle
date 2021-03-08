c0b8cef98b741b4f819a1e55820504706cda990a
Rebecca Chen <rebecca061996@gmail.com>
2021-03-08 18:56:45 +0000

    add test for checking user saves successfully

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should save when all fields are provided' do
+    @user =
+      User.new(
+        first_name: 'Rebecca',
+        last_name: 'Chen',
+        email: 'rebecca.chen@gmail.com',
+        password: 'password',
+        password_confirmation: 'password',
+      ).save
+
+    expect(@user).to eq(true)
+  end
+
+  describe 'Validations' do
+  end
 end
