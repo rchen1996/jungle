61d7f0a5a3954e8dd1299e1f63f862d327eaa526
Rebecca Chen <rebecca061996@gmail.com>
2021-03-08 19:45:59 +0000

    update sessions controller to use new method for authentication with credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.authenticate_with_credentials(params[:email], params[:password])
 
-    if user && user.authenticate(params[:password])
+    if user
       session[:user_id] = user.id
       redirect_to '/'
     else
