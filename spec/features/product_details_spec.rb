e6b907e1013042080efcd1f029a2dd31626563ca
Rebecca Chen <rebecca061996@gmail.com>
2021-03-08 22:44:38 +0000

    setup product details feature spec

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature 'ProductDetails', type: :feature do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99,
+      )
+    end
+  end
+
+  scenario 'They navigate from the home page to the product page by clicking on a product' do
+    # ACT
+    visit root_path
+
+    # VERIFY
+    expect(page).to have_css 'article.product', count: 10
+  end
 end
