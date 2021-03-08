63b307f2014f7292a2f8deff5500150b95f9976f
Rebecca Chen <rebecca061996@gmail.com>
2021-03-08 22:22:59 +0000

    fix spelling mistake in require

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 require 'capybara/rails'
-require 'capybara/rpsec'
+require 'capybara/rspec'
 require 'capybara/poltergeist'
 
 # Specs flagged with `js: true` will use Capybara's JS driver
