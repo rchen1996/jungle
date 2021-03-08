a6b2d1a5ba114524f7c0a01554712101e617cd84
Rebecca Chen <rebecca061996@gmail.com>
2021-03-08 19:01:41 +0000

    update user model to have validation for password and password confirmation fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :password_digest, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
