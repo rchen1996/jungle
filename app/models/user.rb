98af7a52d081e631092113416119379be2c22767
Rebecca Chen <rebecca061996@gmail.com>
2021-03-08 19:17:14 +0000

    add uniqueness validation for email with case sensitive to be false

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true
   validates :password_confirmation, presence: true
 end
