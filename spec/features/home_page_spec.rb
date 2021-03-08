c7a2ef2d8a34302600c267777098694a6a0e2bfa
Rebecca Chen <rebecca061996@gmail.com>
2021-03-08 22:19:03 +0000

    setup home page feature spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "HomePages", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature 'Visitor navigates to home page', type: :feature, js: true do
+  scenario 'They see all products' do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    save_screenshot
+  end
 end
