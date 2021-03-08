9553a1dafa5cea40d16f75083c1692f51afa7a29
Rebecca Chen <rebecca061996@gmail.com>
2021-03-08 22:46:01 +0000

    set js to true

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'ProductDetails', type: :feature do
+RSpec.feature 'ProductDetails', type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
