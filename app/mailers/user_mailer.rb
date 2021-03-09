456d67a4d152b27734cd2cb1bbff72be01bbd941
Rebecca Chen <rebecca061996@gmail.com>
2021-03-09 17:57:54 +0000

    update subject line

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def order_email(user, order)
     @user = user
     @order = order
-    mail(to: @user.email, subject: "Order ##{@order.id} from Jungle")
+    mail(to: @user.email, subject: "Order Id ##{@order.id} from Jungle")
   end
 end
