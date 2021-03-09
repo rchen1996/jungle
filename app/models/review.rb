516cf8eb84ef7c1f5fae7549889a69a22cbdf2be
Rebecca Chen <rebecca061996@gmail.com>
2021-03-09 20:37:19 +0000

    add validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validates :product, presence: true
+  validates :user, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
 end
