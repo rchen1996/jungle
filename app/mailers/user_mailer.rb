b9ce41fe32454d0c1742cad69611349636a892af
Rebecca Chen <rebecca061996@gmail.com>
2021-03-09 17:54:09 +0000

    create method for sending order email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  def order_email(user, order)
+    @user = user
+    @order = order
+    mail(to: @user.email, subject: "Order ##{@order.id} from Jungle")
+  end
 end
