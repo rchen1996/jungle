66e058ca41f1679a65c8bcb663d65a6db4e86bed
Rebecca Chen <rebecca061996@gmail.com>
2021-03-09 18:50:58 +0000

    create preview for order email

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def order_email
+    products = Product.all
+    UserMailer.order_email(Order.first, products)
+  end
 end
