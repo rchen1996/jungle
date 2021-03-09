289594e5de85f92729464f6f7f19880ebe62f48f
Rebecca Chen <rebecca061996@gmail.com>
2021-03-09 00:01:32 +0000

    implement spec for user login

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'UserLogins', type: :feature do
+RSpec.feature 'UserLogins', type: :feature, js: true do
   before :each do
     @user =
       User.create! first_name: 'Alice',
@@ -14,6 +14,13 @@ RSpec.feature 'UserLogins', type: :feature do
     # ACT
     visit login_path
 
-    save_screenshot
+    fill_in 'email', with: 'alice@wonderland.com'
+    fill_in 'password', with: 'password'
+
+    find_button('Login').click
+
+    sleep 1
+
+    expect(find('nav ul.navbar-right')).to have_text 'Logout'
   end
 end
