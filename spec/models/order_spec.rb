0384608479c387083ef14cc817f19433bca46ba6
Rebecca Chen <rebecca061996@gmail.com>
2021-03-10 01:59:30 +0000

    update spec for deduct quantity to work with multiple line items

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe Order, type: :model do
       @order =
         Order.new(
           email: 'alice@wonderland.com',
-          total_cents: 5000,
+          total_cents: 8449,
           stripe_charge_id: 1_234_567_890,
         )
 
@@ -46,6 +46,13 @@ RSpec.describe Order, type: :model do
         total_price: @product2.price * 2,
       )
 
+      @order.line_items.new(
+        product: @product1,
+        quantity: 1,
+        item_price: @product1.price,
+        total_price: @product1.price * 1,
+      )
+
       @order.save!
 
       # reload products to have updated quantities
@@ -54,6 +61,7 @@ RSpec.describe Order, type: :model do
 
       # assert quantity values
       expect(@product2.quantity).to eq(6)
+      expect(@product1.quantity).to eq(3)
     end
 
     xit 'does not deduct quantity from products that are not in the order' do
