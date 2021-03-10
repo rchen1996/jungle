c97a0c2f45909541d860744c2b5b8e9924abee98
Rebecca Chen <rebecca061996@gmail.com>
2021-03-10 01:34:32 +0000

    create initial setup for order tests

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'After creation' do
+    before :each do
+      @category1 = Category.create! name: 'Apparel'
+
+      @product1 =
+        Product.create!(
+          {
+            name: 'Hipster Hat',
+            description: Faker::Hipster.paragraph(4),
+            image: open_asset('apparel3.jpg'),
+            quantity: 4,
+            price: 34.49,
+            category_id: 1,
+          },
+        )
+      @product2 =
+        Product.create!(
+          {
+            name: 'Hipster Socks',
+            description: Faker::Hipster.paragraph(4),
+            image: open_asset('apparel4.jpg'),
+            quantity: 8,
+            price: 25.00,
+            category_id: 1,
+          },
+        )
+    end
+
+    xit 'deducts quantity from products based on their line item quantities' do
+    end
+
+    xit 'does not deduct quantity from products that are not in the order' do
+    end
+  end
 end
