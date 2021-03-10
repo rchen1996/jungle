2b89bbf7bccfd9298f95bd63368ec2a0ff88b726
Rebecca Chen <rebecca061996@gmail.com>
2021-03-10 03:15:45 +0000

    update handling of http aunthetication to use ENV correctly

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
-  http_basic_authenticate_with name: Rails.configuration.admin[:username],
-                               password: Rails.configuration.admin[:password]
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'],
+                               password: ENV['ADMIN_PASSWORD']
 
   def index
     @categories = Category.order(id: :desc).all
