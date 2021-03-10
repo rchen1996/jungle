e51edfe901c4c890cb0afe032c55cea08c60269b
Rebecca Chen <rebecca061996@gmail.com>
2021-03-10 02:05:13 +0000

    create test for checking that products not in an order don't get their quantities deducted and update the way products are created before each test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,25 +6,23 @@ RSpec.describe Order, type: :model do
       @category1 = Category.create! name: 'Apparel'
 
       @product1 =
-        Product.create!(
+        @category1.products.create!(
           {
             name: 'Hipster Hat',
             description: Faker::Hipster.paragraph(4),
             image: open_asset('apparel3.jpg'),
             quantity: 4,
             price: 34.49,
-            category_id: 1,
           },
         )
       @product2 =
-        Product.create!(
+        @category1.products.create!(
           {
             name: 'Hipster Socks',
             description: Faker::Hipster.paragraph(4),
             image: open_asset('apparel4.jpg'),
             quantity: 8,
             price: 25.00,
-            category_id: 1,
           },
         )
     end
@@ -64,7 +62,32 @@ RSpec.describe Order, type: :model do
       expect(@product1.quantity).to eq(3)
     end
 
-    xit 'does not deduct quantity from products that are not in the order' do
+    it 'does not deduct quantity from products that are not in the order' do
+      # initialize order
+      @order =
+        Order.new(
+          email: 'alice@wonderland.com',
+          total_cents: 8449,
+          stripe_charge_id: 1_234_567_890,
+        )
+
+      # build line items on order
+      @order.line_items.new(
+        product: @product2,
+        quantity: 2,
+        item_price: @product2.price,
+        total_price: @product2.price * 2,
+      )
+
+      @order.save!
+
+      # reload products to have updated quantities
+      @product1.reload
+      @product2.reload
+
+      # assert quantity values
+      expect(@product2.quantity).to eq(6)
+      expect(@product1.quantity).to eq(4)
     end
   end
 end
